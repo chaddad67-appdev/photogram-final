a0e83a3a498dda69cd35805b8d6624a798d7a98a
chaddad67 <chaddad@chicagobooth.edu>
2023-02-25 02:01:16 +0000

    v inicial photogram modelo de datos creado

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+
+  has_many(:photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+  has_many(:follow_requests_sent, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:follow_requests_received, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+
+  has_many(:recipients, { :through => :follow_requests_sent, :source => :recipient })
+  has_many(:senders, { :through => :follow_requests_received, :source => :sender })
+
+end
